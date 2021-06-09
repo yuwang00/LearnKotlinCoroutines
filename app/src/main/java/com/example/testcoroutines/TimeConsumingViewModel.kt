634ac4b3044e7f855715654d294c5f1e792a6afd
mathewchen <136214454@qq.com>
2021-06-09 19:04:55 +0800

    串行执行多个耗时任务完成并计算

--- app/src/main/java/com/example/testcoroutines/TimeConsumingViewModel.kt
+++ app/src/main/java/com/example/testcoroutines/TimeConsumingViewModel.kt
@@ -27,15 +27,17 @@ class TimeConsumingViewModel : ViewModel() {
         viewModelScope.launch {
             Log.i(TAG, "launch before:$isMainThread")
             val result = sleepThreeSec()
+            val secResult = sleepTwoSec()
             runInMainThread()
-            Log.i(TAG, "launch after:$isMainThread,result:$result")
+            Log.i(TAG, "launch after:$isMainThread,result:${result + secResult}")
         }
 
         Log.i(TAG, "after:$isMainThread")
-        // launch before:true
-        // after:true
-        // runInMainThread:true
-        // launch after:true,result:1
+        // 串行执行，注意时间戳
+        // 2021-06-09 19:03:39.087 14178-14178/com.example.testcoroutines I/TimeConsumingViewModel: launch before:true
+        // 2021-06-09 19:03:39.101 14178-14178/com.example.testcoroutines I/TimeConsumingViewModel: after:true
+        // 2021-06-09 19:03:44.123 14178-14178/com.example.testcoroutines I/TimeConsumingViewModel: runInMainThread:true
+        // 2021-06-09 19:03:44.124 14178-14178/com.example.testcoroutines I/TimeConsumingViewModel: launch after:true,result:3
     }
 
     private suspend fun sleepThreeSec(): Int {
@@ -43,6 +45,14 @@ class TimeConsumingViewModel : ViewModel() {
             delay(3000)
             // 假设耗时三秒计算出结果
             1
+        }
+    }
+
+    private suspend fun sleepTwoSec(): Int {
+        return withContext(Dispatchers.IO) {
+            delay(2000)
+            // 假设耗时三秒计算出结果
+            2
         }
     }
 
