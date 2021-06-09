af1c15397d6720c15c97e6ce0b927b690408e206
mathewchen <136214454@qq.com>
2021-06-09 19:17:39 +0800

    错误使用async并行执行多个耗时任务完成并计算

--- app/src/main/java/com/example/testcoroutines/TimeConsumingViewModel.kt
+++ app/src/main/java/com/example/testcoroutines/TimeConsumingViewModel.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -26,18 +27,18 @@ class TimeConsumingViewModel : ViewModel() {
     fun timeConsumingMethod() {
         viewModelScope.launch {
             Log.i(TAG, "launch before:$isMainThread")
-            val result = sleepThreeSec()
-            val secResult = sleepTwoSec()
+            val result = async { sleepThreeSec() }
+            val secResult = async { sleepTwoSec() }
             runInMainThread()
-            Log.i(TAG, "launch after:$isMainThread,result:${result + secResult}")
+            Log.i(TAG, "launch after:$isMainThread")
         }
 
         Log.i(TAG, "after:$isMainThread")
-        // 串行执行，注意时间戳
-        // 2021-06-09 19:03:39.087 14178-14178/com.example.testcoroutines I/TimeConsumingViewModel: launch before:true
-        // 2021-06-09 19:03:39.101 14178-14178/com.example.testcoroutines I/TimeConsumingViewModel: after:true
-        // 2021-06-09 19:03:44.123 14178-14178/com.example.testcoroutines I/TimeConsumingViewModel: runInMainThread:true
-        // 2021-06-09 19:03:44.124 14178-14178/com.example.testcoroutines I/TimeConsumingViewModel: launch after:true,result:3
+        // 并未执行，注意时间戳
+        // 2021-06-09 19:16:07.630 14637-14637/com.example.testcoroutines I/TimeConsumingViewModel: launch before:true
+        // 2021-06-09 19:16:07.646 14637-14637/com.example.testcoroutines I/TimeConsumingViewModel: runInMainThread:true
+        // 2021-06-09 19:16:07.646 14637-14637/com.example.testcoroutines I/TimeConsumingViewModel: launch after:true
+        // 2021-06-09 19:16:07.682 14637-14637/com.example.testcoroutines I/TimeConsumingViewModel: after:true
     }
 
     private suspend fun sleepThreeSec(): Int {
@@ -51,7 +52,7 @@ class TimeConsumingViewModel : ViewModel() {
     private suspend fun sleepTwoSec(): Int {
         return withContext(Dispatchers.IO) {
             delay(2000)
-            // 假设耗时三秒计算出结果
+            // 假设耗时两秒计算出结果
             2
         }
     }
